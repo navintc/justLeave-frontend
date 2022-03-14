55dc4e020eebeeb535c9c1265293b93e0f5daa51
navintc <navin.thamindu@gmail.com>
2022-03-14 13:04:34 +0530

    signin update

--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState }  from "react";
 import axios from "axios";
+import {Button, Col, Container, Form, Row} from "react-bootstrap";
 
 
 const baseURL = "http://127.0.0.1:8000/api/leaves/";
@@ -17,14 +18,27 @@ const Signin = (props) => {
     console.log(leaves);
 
     return (
-        <div>
-            {leaves.map((item, index)=>
-                <div key={item.leaveID}>
-                    <h1>{item.leaveID}</h1>
-                </div>
-            )}
+        <div class={"content-space main-component"}>
+            <Container class={"my-auto"}>
+                <Row className="justify-content-md-center">
+                    <Col xs={3}>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
 
-
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
